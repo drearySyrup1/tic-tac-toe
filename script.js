379b3dfcc875a6598b2b0a922593963548b02085
Aurimas <gaizauskasaurimas@gmail.com>
2022-08-11 16:12:28 +0500

    eslint

--- script.js
+++ script.js
@@ -1,216 +1,196 @@
-const gameBoard = (function (){
-    let _board = Array(9);
+/* eslint-disable no-use-before-define */
+const gameBoard = (() => {
+  let _board = Array(9);
 
-    function setField (index, value) {
-        _board[index] = value;
-    }
+  function setField(index, value) {
+    _board[index] = value;
+  }
 
-    function getField (index) {
-        return _board[index]
-    }
+  function getField(index) {
+    return _board[index];
+  }
 
-    function a() {
-        console.log(_board);
-    }
-    
-    function resetBoard() {
-        _board = Array(9);
-    }
+  function resetBoard() {
+    _board = Array(9);
+  }
 
-    return {a, setField, getField, resetBoard}
-
+  return {
+    setField, getField, resetBoard,
+  };
 })();
 
+const AI = (() => {
+  function getMove() {
+    return Math.floor(Math.random() * 9);
+  }
 
-function Player(type) {
-    return {
-        type
-    }
-};
-
-const AI = (function (){
-    function getMove() {
-        return Math.floor(Math.random() * 9);
-    }
-
-    return {getMove}
+  return { getMove };
 })();
 
+const gameController = (() => {
+  // create players
+  let player1 = document.querySelector('input[name="player"]:checked').id;
+  let gameMode = document.querySelector('input[name="game-mode"]:checked').id;
+  let player2 = (player1 === 'x') ? 'o' : 'x';
+  let turn = player1;
+  let winner = null;
 
-const gameController = (function () {
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    //create players
-    let player1 = document.querySelector('input[name="player"]:checked').id;
-    let gameMode = document.querySelector('input[name="game-mode"]:checked').id;
-    let player2 = (player1 === 'x') ? 'o' : 'x';
-    let turn = player1;
-    let winner = null;
+  function changePlayer(value) {
+    player1 = value;
+    player2 = (player1 === 'x') ? 'o' : 'x';
+    displayController.updateTurn(player1);
+    turn = player1;
+    restart();
+  }
 
-    const winningPositions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
+  function changeGameMode(value) {
+    gameMode = value;
+    restart();
+  }
 
-    function changePlayer(value) {
-        player1 = value;
-        player2 = (player1 === 'x') ? 'o' : 'x';
-        displayController.updateTurn(player1)
-        turn = player1;
-        restart();
+  function cellHandler(e) {
+    if (winner !== null) return;
+    if (!e.target.classList.contains('cell')) return;
+    if (!checkIfValidMove(e.target)) return;
+
+    const correctCell = e.target.querySelector(`[data-type="${turn}"]`);
+    const index = parseInt(e.target.dataset.id, 10);
+    correctCell.classList.remove('hide');
+    gameBoard.setField(index, turn);
+
+    if (checkIfWon(turn)) {
+      displayController.showWinner(turn);
+      winner = turn;
+      return;
     }
-
-    function changeGameMode(value) {
-        gameMode = value;
-        restart();
+    if (checkIfDraw()) {
+      displayController.showDraw();
+      return;
     }
+    if (gameMode !== 'AI') {
+      swapTurn();
+    } else {
+      let cellNumber = AI.getMove();
 
-    function cellHandler(e) {
-        if (winner !== null) return;
-        if (!e.target.classList.contains('cell')) return;
-        if (!checkIfValidMove(e.target)) return;
+      while (gameBoard.getField(cellNumber) !== undefined) {
+        cellNumber = AI.getMove();
+      }
 
-        const correctCell = e.target.querySelector(`[data-type="${turn}"]`)
-        const index = parseInt(e.target.dataset.id);
-        correctCell.classList.remove('hide')
-        gameBoard.setField(index, turn);
-
-        if (checkIfWon(turn)) {
-            displayController.showWinner(turn);
-            winner = turn;
-            return;
-        } else if (checkIfDraw()) {
-            displayController.showDraw();
-            return;
-        }
-        if (gameMode !== 'AI') {
-            swapTurn();
-        } else {
-            let cellNumber = AI.getMove();
-
-            while (gameBoard.getField(cellNumber) !== undefined) {
-                cellNumber = AI.getMove();
-            }
-
-            const cell = document.querySelector(`#board [data-id="${cellNumber}"]`)
-            gameBoard.setField(cellNumber, player2)
-            const toHide = cell.querySelector(`[data-type="${player2}"]`)
-            toHide.classList.remove('hide');
-            if (checkIfWon(player2)) {
-                displayController.showWinner(player2);
-                return;
-            }
-        }
-        // gameBoard.a(); 
+      const cell = document.querySelector(`#board [data-id="${cellNumber}"]`);
+      gameBoard.setField(cellNumber, player2);
+      const toHide = cell.querySelector(`[data-type="${player2}"]`);
+      toHide.classList.remove('hide');
+      if (checkIfWon(player2)) {
+        displayController.showWinner(player2);
+      }
     }
+  }
 
-    function checkIfDraw() {
-        let draw = true;
-        for (let i = 0; i < 9; i++) {
-            if (gameBoard.getField(i) == undefined) {
-                draw = false;
-                break;
-            }
-        }
-        return draw;
+  function checkIfDraw() {
+    let draw = true;
+    for (let i = 0; i < 9; i += 1) {
+      if (gameBoard.getField(i) === undefined) {
+        draw = false;
+        break;
+      }
     }
+    return draw;
+  }
 
-    function swapTurn() {
-        turn = (turn === 'x') ? 'o' : 'x';
-        displayController.updateTurn(turn);
-    }
+  function swapTurn() {
+    turn = (turn === 'x') ? 'o' : 'x';
+    displayController.updateTurn(turn);
+  }
 
-    function checkIfValidMove(cell) {
-        const id = cell.dataset.id;
-        return gameBoard.getField(id) === undefined;
-    }
+  function checkIfValidMove(cell) {
+    const { id } = cell.dataset;
+    return gameBoard.getField(id) === undefined;
+  }
 
-    function checkIfWon(turn) {
-        let won = false;
-        winningPositions.forEach(posArray => {
-            if (posArray.every(
-                pos => {
-                    return gameBoard.getField(pos) === turn;
-                }
-            )) won = true;
-        })
+  function checkIfWon(whosTurn) {
+    let won = false;
+    winningPositions.forEach((posArray) => {
+      if (posArray.every((pos) => gameBoard.getField(pos) === whosTurn)) won = true;
+    });
+    return won;
+  }
 
-        return won;
-    }
+  function restart() {
+    displayController.resetCells();
+    gameBoard.resetBoard();
+    displayController.updateTurn(turn);
+    displayController.removeButton();
+    winner = null;
+  }
 
-    function restart(e) {
-        displayController.resetCells();
-        gameBoard.resetBoard();
-        displayController.updateTurn(turn)
-        displayController.removeButton();
-        winner = null;
-    }
-
-    return {changePlayer, changeGameMode, cellHandler, restart}
-
+  return {
+    changePlayer, changeGameMode, cellHandler, restart,
+  };
 })();
 
+const displayController = (() => {
+  const _cells = document.querySelectorAll('.cell');
+  const turn = document.getElementById('turn');
+  const playerTypeRadioButtons = document.getElementsByName('player');
+  const gameModeRadioButtons = document.getElementsByName('game-mode');
 
+  function showRestartButton() {
+    const restart = document.createElement('button');
+    const board = document.getElementById('board');
+    const main = board.parentNode;
+    restart.innerText = 'Restart';
+    restart.id = 'removeButton';
+    restart.classList.add('btn');
+    restart.addEventListener('click', gameController.restart);
+    main.insertBefore(restart, board);
+  }
+  function resetCells() {
+    _cells.forEach((cell) => {
+      Array.from(cell.children).forEach((child) => child.classList.add('hide'));
+    });
+  }
 
-const displayController = (function () {
-    const _cells = document.querySelectorAll('.cell');
-    const turn = document.getElementById('turn')
-    
-    const playerTypeRadioButtons = document.getElementsByName('player');
-    const gameModeRadioButtons = document.getElementsByName('game-mode');
+  function updateTurn(value) {
+    turn.innerText = `${value.toUpperCase()} turn`;
+  }
 
-    playerTypeRadioButtons.forEach(btn => btn.addEventListener('click', () => {
-        if (btn.checked) gameController.changePlayer(btn.id)
-    }))
+  function showWinner(winner) {
+    turn.innerText = `${winner.toUpperCase()} has WON!`;
+    showRestartButton();
+  }
 
-    gameModeRadioButtons.forEach(btn => btn.addEventListener('click', () => {
-        if (btn.checked) gameController.changeGameMode(btn.id)
-    }))
+  function showDraw() {
+    turn.innerText = 'It\'s a DRAW!';
+    showRestartButton();
+  }
 
+  function removeButton() {
+    const button = document.getElementById('removeButton');
+    button.parentElement.removeChild(button);
+  }
 
-    function resetCells() {
-        _cells.forEach(cell => {
-            Array.from(cell.children).forEach(child => child.classList.add('hide'));
-        })
-    }
+  playerTypeRadioButtons.forEach((btn) => btn.addEventListener('click', () => {
+    if (btn.checked) gameController.changePlayer(btn.id);
+  }));
 
-    function updateTurn(value) {
-        turn.innerText = `${value.toUpperCase()} turn`
-    }
+  gameModeRadioButtons.forEach((btn) => btn.addEventListener('click', () => {
+    if (btn.checked) gameController.changeGameMode(btn.id);
+  }));
 
-    function showWinner(winner) {
-        turn.innerText = `${winner.toUpperCase()} has WON!`
-        showRestartButton();
-    }
+  _cells.forEach((cell) => cell.addEventListener('click', gameController.cellHandler, true));
 
-    function showDraw() {
-        turn.innerText = `It's a DRAW!`
-        showRestartButton();
-    }
-
-    function removeButton() {
-        const button = document.getElementById('removeButton');
-        button.parentElement.removeChild(button)
-    }
-
-    function showRestartButton() {
-        const restart = document.createElement('button');
-        const board = document.getElementById('board');
-        const main = board.parentNode;
-        restart.innerText = 'Restart';
-        restart.id = 'removeButton'
-        restart.classList.add('btn')
-        restart.addEventListener('click', gameController.restart)
-        main.insertBefore(restart, board)
-    }
-
-    _cells.forEach(cell => cell.addEventListener('click', gameController.cellHandler, true))
-
-    return {updateTurn, showWinner, resetCells, showDraw, removeButton}
-
+  return {
+    updateTurn, showWinner, resetCells, showDraw, removeButton,
+  };
 })();
